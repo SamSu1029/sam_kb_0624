b4b670623b393e1bc0afad248689dfeeab2db4c6
SamSu1029 <1305669102@qq.com>
2026-09-11 21:45:14 +0800

    1、删除了一些文件 2、调整node_pdf_to_md代码，添加总结

--- backup/kb_main/import_process/nodes/node_pdf_to_md.py
+++ backup/kb_main/import_process/nodes/node_pdf_to_md.py
@@ -1,7 +1,10 @@
+
 import shutil
 import time
 import zipfile
 from pathlib import Path
+
+
 
 from kb_main.config.config import MineruConfig
 from kb_main.import_process.base import NodeBase
@@ -20,153 +23,172 @@ class NodePDFToMD(NodeBase):
 
     def process(self, state: ImportGraphState):
 
-        # 1 防御性编程，输入属性是否存在
+        # 1、防御性校验
         pdf_path = state.get("pdf_path", "")
         if not pdf_path:
-            logger.error("pdf_path路径未提供")
-            raise ValueError("pdf_path路径未提供")
-
+            logger.error("未上传pdf_path")
+            raise Exception("未上传pdf_path")
         pdf_path_obj = Path(pdf_path)
         if not pdf_path_obj.exists():
-            logger.error("pdf_path路径不存在")
-            raise ValueError("pdf_path路径不存在")
+            logger.error(f"pdf_path路径不存在{pdf_path_obj}")
+            raise Exception(f"pdf_path路径不存在{pdf_path_obj}")
 
         local_dir = state.get("local_dir", "")
         if not local_dir:
-            logger.error("local_dir路径未提供")
-            raise ValueError("local_dir路径未提供")
+            logger.error("未上传local_dir")
+            raise Exception("未上传local_dir")
         local_dir_obj = Path(local_dir)
         if not local_dir_obj.exists():
             local_dir_obj.mkdir(parents=True, exist_ok=True)
-            logger.info("local_dir路径不存在，已创建")
+            logger.info(f"{local_dir_obj}路径不存在,已创建")
 
-        # 2 上传pdf文件到mineru，拿到batch_id
+        # 2、上传pdf到mineru
         import requests
 
         token = MineruConfig.mineru_token
         url = "https://mineru.net/api/v4/file-urls/batch"
         header = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}"
+            "Authorization": f"Bearer {token}",
         }
         data = {
-            "files": [
-                {"name": f"{pdf_path_obj.name}", "data_id": "abcd"}
-            ],
-            "model_version": "vlm"
+            "files": [{"name": f"{pdf_path_obj.name}", "data_id": "abcd"}],
+            "model_version": "vlm",
         }
         file_path = [pdf_path]
 
         response = requests.post(url, headers=header, json=data)
         if response.status_code != 200:
-            logger.error("上传pdf文件请求失败")
-            raise Exception("上传pdf文件请求失败")
-        logger.info("上传pdf文件请求成功")
-        result = response.json()
-        if result["code"] != 0:
-            logger.error("上传pdf文件请求数据失败")
-            raise Exception("上传pdf文件请求数据失败")
-        logger.info("上传pdf文件请求数据成功")
-        batch_id = result["data"]["batch_id"]
-        urls = result["data"]["file_urls"]
+            logger.error(f"上传pdf网络请求失败：{response.status_code}")
+            raise Exception(f"上传pdf网络请求失败：{response.status_code}")
+        logger.info(f"上传pdf网络请求成功")
+        upload_result = response.json()
+        if upload_result["code"] != 0:
+            logger.error(f"上传pdf业务请求失败：{upload_result.get('msg')}")
+            raise Exception(f"上传pdf业务请求失败：{upload_result.get('msg')}")
+        logger.info("上传pdf业务请求成功")
+        batch_id = upload_result["data"]["batch_id"]
+        urls = upload_result["data"]["file_urls"]
 
         for i in range(0, len(urls)):
-            with open(file_path[i], 'rb') as f:
+            with open(file_path[i], "rb") as f:
                 res_upload = requests.put(urls[i], data=f)
                 if res_upload.status_code == 200:
-                    logger.info(f"{urls[i]} 上传成功")
+                    logger.info(f"{urls[i]} 文件上传成功")
                 else:
-                    logger.error(f"{urls[i]} 上传失败")
+                    logger.error(f"{urls[i]} 文件上传失败")
+                    raise Exception(f"{urls[i]} 文件上传失败")
 
-        # 3、从mineru获取zip文件的下载地址
+        # 3、获取zip下载url
         token = MineruConfig.mineru_token
         batch_id = batch_id
         url = f"https://mineru.net/api/v4/extract-results/batch/{batch_id}"
         header = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}"
+            "Authorization": f"Bearer {token}",
         }
 
+        # 轮询
         max_time = 300
         used_time = 0
         while True:
             start_time = time.time()
-            res = requests.get(url, headers=header)
             try:
-                if res.status_code != 200:
-                    logger.error("获取zip文件下载地址请求失败")
-                    raise Exception("获取zip文件下载地址请求失败")
-                logger.info("获取zip文件下载地址请求成功")
-                result = res.json()
-                if result["code"] != 0:
-                    logger.error("获取zip文件下载地址请求数据失败")
-                    raise Exception("获取zip文件下载地址请求数据失败")
-                logger.info("获取zip文件下载地址请求数据成功")
-
-                if result["data"]["extract_result"][0]["state"] != "done":
-                    logger.info("文件处理中,请稍候...")
+                download_url_response = requests.get(url, headers=header)
+                if download_url_response.status_code != 200:
+                    logger.error(
+                        f"获取下载url网络请求失败：{download_url_response.status_code}"
+                    )
+                    raise Exception(
+                        f"获取下载url网络请求失败：{download_url_response.status_code}"
+                    )
+                download_url_res = download_url_response.json()
+                if download_url_res["code"] != 0:
+                    logger.error(
+                        f"获取下载url业务请求失败：{download_url_res.get('msg')}"
+                    )
+                    raise Exception(
+                        f"获取下载url业务请求失败：{download_url_res.get('msg')}"
+                    )
+                data = download_url_res["data"]
+                if data["extract_result"][0]["state"] != "done":
+                    logger.info(
+                        f"pdf转md处理中，状态：{data['extract_result'][0]['state']}"
+                    )
                     end_time = time.time()
-                    used_time += (end_time - start_time)
+                    used_time += end_time - start_time
                     if used_time > max_time:
-                        logger.error("文件处理超时")
-                        raise Exception("文件处理超时")
-                    time.sleep(3)
-                    used_time += 3
+                        logger.error("获取下载url超时")
+                        raise Exception("获取下载url超时")
+                    time.sleep(2)
+                    used_time += 2
                     continue
-                full_zip_url = result["data"]["extract_result"][0]["full_zip_url"]
-                logger.info(f"zip文件下载地址获取成功：{full_zip_url}")
+
+                full_zip_url = data["extract_result"][0]["full_zip_url"]
+                logger.info(f"获取下载url成功:{full_zip_url}")
                 break
             except Exception as e:
-                logger.error(f"获取处理结果请求异常，正在重试：{e}")
+                logger.error(f"获取下载url失败:{e}")
                 end_time = time.time()
-                used_time += (end_time - start_time)
+                used_time += end_time - start_time
                 if used_time > max_time:
-                    logger.error("获取zip文件下载地址超时")
-                    raise Exception("获取zip文件下载地址超时")
-                time.sleep(3)
-                used_time += 3
+                    logger.error("获取下载url超时")
+                    raise Exception("获取下载url超时")
+                time.sleep(2)
+                used_time += 2
                 continue
 
-        # 4、下载文件到本地
-        response=requests.get(full_zip_url)
-        if response.status_code!=200:
-            logger.error("下载zip文件请求失败")
-            raise Exception("下载zip文件请求失败")
-        logger.info("下载zip文件请求成功")
-        content=response.content
-        md_zip_file_path_obj=local_dir_obj / f"{pdf_path_obj.stem}.zip"
-        with open(md_zip_file_path_obj,'wb') as f:
-            f.write(content)
+        # 4、下载zip文件
+        download_zip_response = requests.get(full_zip_url)
+        if download_zip_response.status_code != 200:
+            logger.error(
+                f"下载zip文件网络请求失败：{download_zip_response.status_code}"
+            )
+            raise Exception(
+                f"下载zip文件网络请求失败：{download_zip_response.status_code}"
+            )
+        logger.info(f"下载zip文件网络请求成功")
+        download_zip_content = download_zip_response.content
 
-#         解压文件
-        #解压目录
-        unzip_dir_path_obj = local_dir_obj/pdf_path_obj.stem
-        if  unzip_dir_path_obj.exists():
-            shutil.rmtree(unzip_dir_path_obj)
-        unzip_dir_path_obj.mkdir(parents=True, exist_ok=True)
+        download_zip_path_obj = local_dir_obj / f"{pdf_path_obj.stem}.zip"
+        with open(download_zip_path_obj, "wb") as f:
+            f.write(download_zip_content)
+        logger.info(f"zip文件写入本地成功：{download_zip_path_obj}")
 
-        with zipfile.ZipFile(md_zip_file_path_obj, 'r') as zf:
-            zf.extractall(unzip_dir_path_obj)
+        # 5、解压zip文件
+        unzip_path_obj = local_dir_obj / pdf_path_obj.stem
+        if unzip_path_obj.exists():
+            shutil.rmtree(unzip_path_obj)
+            logger.info(f"解压目录存在，已删除:{unzip_path_obj}")
+        unzip_path_obj.mkdir(parents=True, exist_ok=True)
+        logger.info(f"解压目录创建成功:{unzip_path_obj}")
 
-        # 重命名
-        origin_md_path_obj = unzip_dir_path_obj / "full.md"
-        new_md_path_obj=origin_md_path_obj.with_name(f"{pdf_path_obj.stem}.md")
+        with zipfile.ZipFile(download_zip_path_obj, "r") as z:
+            z.extractall(unzip_path_obj)
+        logger.info(f"zip文件解压成功")
+
+        # 6、重命名
+        origin_md_path_obj = unzip_path_obj / "full.md"
+        new_md_path_obj = origin_md_path_obj.with_name(
+            f"{pdf_path_obj.stem}.md"
+        )  # 传文件名，返回整个path对象
         origin_md_path_obj.rename(new_md_path_obj)
+        logger.info(f"md文件重命名成功:{new_md_path_obj}")
 
-        with open(new_md_path_obj, 'r', encoding='utf-8') as f:
-            md_content = f.read()
+        with open(new_md_path_obj,'r',encoding='utf-8') as f:
+            md_content=f.read()
 
-        return {"md_content": md_content, "md_path": str(new_md_path_obj)}
+        return {"md_path": str(new_md_path_obj),
+                "md_content":md_content
+                }
 
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     node = NodePDFToMD()
     state = init_state = {
         "pdf_path": r"D:\data\output\hak180产品安全手册.pdf",
-        "local_dir": r"D:\data\output"
+        "local_dir": r"D:\data\output",
     }
     res = node(state)
     logger.info(json_format(res))
--- kb_main/config/config.py
+++ kb_main/config/config.py
@@ -38,3 +38,10 @@ class MinIoConfig:
     minio_img_dir = get_required_env("MINIO_IMG_DIR")
 
 
+# config/config.py
+class EmbeddingConfig:
+    bge_m3_path=get_required_env("BGE_M3_PATH")
+    bge_m3=get_required_env("BGE_M3")
+    bge_device=get_required_env("BGE_DEVICE")
+    # 特殊处理：将.env中的1/0转为布尔值，兼容常见的数字/字符串格式
+    bge_fp16=get_required_env("BGE_FP16") in ("1", "True", "true", 1)
